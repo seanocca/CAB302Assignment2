5f823fd3810f4e09bd60383874a734cf1ee233fe
seanocca <32288543+seanocca@users.noreply.github.com>
2018-05-27 12:57:23 +1000

    Completed Stock Class
    Completed Stock class that corresponds to the Stock Test class

---
+++ src/Stock/Stock.java
@@ -0,0 +1,94 @@
+package Stock;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Stock {
+	
+	private ArrayList<Item> items = new ArrayList<Item>();
+
+	/**
+	 * Initializes the class with nothing in it
+	 * @author Sean O'Connell
+	 */
+	public Stock() {
+	}
+	
+	/**
+	 * Returns the Stock as a Item array
+	 * @return
+	 * @author Sean O'Connell
+	 */
+	public ArrayList<Item> GetStockArray(){
+		return items;
+	}
+	
+	/**
+	 * Returns the size of the Stock
+	 * @return
+	 * @author Sean O'Connell
+	 */
+	public int GetSize() {
+		return items.size();
+	}
+	
+	/**
+	 * Adds an Item to the Stock list
+	 * @param item
+	 * @author Sean O'Connell
+	 */
+	public void add(Item item) {
+		items.add(item);
+	}
+	
+	/**
+	 * Adds all the Items from an array to the Stock
+	 * @param items
+	 * @author Sean O'Connell
+	 */
+	public void addAll(ArrayList<Item> items) {
+		this.items = items;
+	}
+	
+	/**
+	 * Sorts the Stock by temperature of each Item
+	 * @author Sean O'Connell
+	 */
+	public void sortByTemp() {
+		if (GetSize() > 0) {
+			Collections.sort(GetStockArray(), new Comparator<Item>() {
+				@Override
+				public int compare(final Item object1, final Item object2) {
+					return  Integer.valueOf((int)object1.GetTemperature()).compareTo((int)object2.GetTemperature());
+				}
+			});
+		}	
+	}
+	
+	/**
+	 * Returns the number of duplicates of Item in the Stock
+	 * @param find
+	 * @return count
+	 * @author Sean O'Connell
+	 */
+	public int CountDuplicates(Item find){
+		//Create an array of Strings of Item names 
+		ArrayList<String> list = new ArrayList<String>();
+		for (Item item: GetStockArray()) {
+			list.add(item.GetName());
+		}
+		
+        int count = 0;
+        //Check if the list contains the item name
+        if (list.contains(find.GetName())) {
+        	//Run through the array if so and count number of times it is present
+        	for (Item item: GetStockArray()) {
+        		if (item.GetName() == find.GetName()) {
+        			count++;
+        		}
+        	}
+        }
+        return count;
+    }
+}
